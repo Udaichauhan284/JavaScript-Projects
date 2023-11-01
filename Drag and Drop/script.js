a9fa7f1b0e6a65655fd3d166b0e63c6ddbdf517f
Udai Chauhan <udaichauhan284@gmail.com>
2023-11-01 15:59:55 +0530

    Adding Drag and Drop

---
+++ Drag and Drop/script.js
@@ -0,0 +1,32 @@
+const lists = document.querySelectorAll('.list');
+const leftBox = document.querySelector('#left');
+const rightBox = document.querySelector('#right');
+
+let selected = null;
+for(let list of lists){
+  list.addEventListener("dragstart", (e)=>{
+    selected = e.target
+  });
+
+  rightBox.addEventListener("dragover", (e)=>{
+    e.preventDefault();
+  });
+
+  rightBox.addEventListener("drop", (e)=>{
+    if(selected){
+      rightBox.appendChild(selected);
+      selected = null;
+    }
+  });
+
+  leftBox.addEventListener("dragover", (e) =>{
+    e.preventDefault();
+  });
+
+  leftBox.addEventListener("drop",(e)=>{
+    if(selected){
+      leftBox.appendChild(selected);
+      selected=null;
+    }
+  });
+}
